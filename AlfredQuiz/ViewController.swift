349ffcb91c43e267c992efaa1ed2aad0266a7eb7
JustinRuan <justin.ruan@alfred.camera>
2021-04-26 15:00:03 +0800

    Initial Commit

---
+++ AlfredQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlfredQuiz
+//
+//  Created by Justin Ruan on 2021/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
